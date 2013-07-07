ead481fba80f774bb6143dd351d8c564d40395bb
cmark89 <cmark89@gmail.com>
2013-07-07 22:26:00 +0900

    Added framework for viewing documents.

--- Eglantine/Engine/Screens/DocumentScreen.cs
+++ Eglantine/Engine/Screens/DocumentScreen.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Eglantine
 {
-	public class DocumentScreen
+	public class DocumentScreen : Screen
 	{
 		// Singletonize me, cap'n!
 		private static DocumentScreen _instance;
@@ -14,15 +14,68 @@ namespace Eglantine
 			get
 			{
 				if(_instance == null)
-					_instance = new DocumentScreen();
+					_instance = new DocumentScreen(null);
 
 				return _instance;
 			}
 		}
 
-		public DocumentScreen ()
-		{
+		private Document thisDocument;
+		private int currentPage;
+		private DocumentScreenGUI window;
 
+		public DocumentScreen (Document doc)
+		{
+			Initialize();
+			SetDocument(doc);
+		}
+
+		public override void Initialize()
+		{
+		}
+
+		public void SetDocument (Document doc)
+		{
+			thisDocument = doc;
+			currentPage = 0;
+			SetupWindow();
+		}
+
+		private void SetupWindow()
+		{
+			window = new DocumentScreenGUI();
+			window.SetPage(thisDocument.Pages[currentPage].Texture, currentPage + 1, thisDocument.Pages.Count);
+		}
+
+		public void GoToPreviousPage()
+		{
+			currentPage--;
+			window.SetPage(thisDocument.Pages[currentPage].Texture, currentPage + 1, thisDocument.Pages.Count);
+		}
+
+		public void GoToNextPage()
+		{
+			currentPage++;
+			window.SetPage(thisDocument.Pages[currentPage].Texture, currentPage + 1, thisDocument.Pages.Count);
+		}
+
+		public void Close()
+		{
+			// Close the window...
+			window = null;
+			this.FlaggedForRemoval = true;
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			if(window != null)
+				window.Update(gameTime);
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			if(window != null)
+				window.Draw(spriteBatch);
 		}
 	}
 }
