45060095474af28a26158195410daa7815e09a65
cmark89 <cmark89@gmail.com>
2013-08-14 21:57:31 +0900

    Added several journal pages.

--- Eglantine/Data/documents.lua
+++ Eglantine/Data/documents.lua
@@ -21,17 +21,19 @@ documents["Blueprints"] = {
 }
 
 documents["Journal"] = {
-	[1] = "Graphics/Docs/journal1",
-	[2] = "Graphics/Docs/journal2",
-	[3] = "Graphics/Docs/journal3",
-	[4] = "Graphics/Docs/journal4",
-	[5] = "Graphics/Docs/journal5",
-	[6] = "Graphics/Docs/journal6",
-	[7] = "Graphics/Docs/journal7",
-	[8] = "Graphics/Docs/journal8",
-	[9] = "Graphics/Docs/journal9",
-	[10] = "Graphics/Docs/journal10",
-	[11] = "Graphics/Docs/journal11"
+	[1] = "Graphics/Docs/journal-1",
+	[2] = "Graphics/Docs/journal0",
+	[3] = "Graphics/Docs/journal1",
+	[4] = "Graphics/Docs/journal2",
+	[5] = "Graphics/Docs/journal3",
+	[6] = "Graphics/Docs/journal4",
+	[7] = "Graphics/Docs/journal5",
+	[8] = "Graphics/Docs/journal6",
+	[9] = "Graphics/Docs/journal7",
+	[10] = "Graphics/Docs/journal8",
+	[11] = "Graphics/Docs/journal9",
+	[12] = "Graphics/Docs/journal10",
+	[13] = "Graphics/Docs/journal11"
 }
 
 documents["Folded Note"] = {
